56e74d5876e11762dee8a599013b971b1f4bd6d5
Dmitry Pyzhov <fiche40@gmail.com>
2020-09-18 14:29:23 +0300

    Ball Basic Mechanic

---
+++ DemonStrikeProject/Assets/Scripts/BallController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+	public const float MaxForce = 500f;
+	public float movementTime = 2f;
+
+	private Rigidbody2D rb;
+
+	private void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() {
+		//if (rb.velocity.magnitude <= 1f) {
+		//	rb.velocity = Vector2.zero;
+		//}
+	}
+
+	public void Launch(float force) {
+		Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		worldPosition.z = 0f;
+
+		Vector2 dir = (worldPosition - transform.position).normalized * -1f;
+		rb.AddForce(dir * force);
+		//rb.velocity = dir * force;
+
+		StartCoroutine(StopMovement());
+	}
+
+	private IEnumerator StopMovement() {
+
+		yield return new WaitForSeconds(movementTime);
+
+		if (rb.velocity.magnitude >= 1f) {
+			rb.velocity = Vector2.zero;
+		}
+	}
+}
---
+++ DemonStrikeProject/Assets/Scripts/SessionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionController : MonoBehaviour
+{
+    public BallController character;
+
+    private float holdDownStartTime;
+
+	void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            holdDownStartTime = Time.time;
+		}
+
+        if (Input.GetMouseButton(0)) {
+            float holdDownTime = Time.time - holdDownStartTime;
+            
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            float holdDownTime = Time.time - holdDownStartTime;
+            character.Launch(CalculateHoldDownForce(holdDownTime));
+        }
+    }
+
+	private float CalculateHoldDownForce(float holdTime) {
+        float maxForceHoldDownTime = 2f;
+        float holdTimeNormalized = Mathf.Clamp01(holdTime / maxForceHoldDownTime);
+        float force = holdTimeNormalized * BallController.MaxForce;
+
+        return force;
+	}
+}
